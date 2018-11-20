c41e24ee0941efacdb731c9672c5be762c38e8ef
dave griffiths <dave@fo.am>
2018-11-20 11:38:43 +0000

    django version changes

--- sonicmap/wsgi.py
+++ sonicmap/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sonicmap.settings")
+import os, sys
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sonicmap.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "sonicmap.settings"
+sys.path.append('/var/www/sonicmap/')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
